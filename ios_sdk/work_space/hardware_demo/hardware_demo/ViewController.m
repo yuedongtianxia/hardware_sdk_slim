e658841589e896fcb35b7e77d449d9b65f776b01
Minster <zhangminke@51yund.com>
2016-10-31 17:52:15 +0800

    save

---
+++ ios_sdk/work_space/hardware_demo/hardware_demo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  hardware_demo
+//
+//  Created by 张旻可 on 2016/10/31.
+//  Copyright © 2016年 yuedong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
