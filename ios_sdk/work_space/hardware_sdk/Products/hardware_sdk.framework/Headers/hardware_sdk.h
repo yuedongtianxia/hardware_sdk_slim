a7bf3d22790416fa6627144b8471e5df39221665
Minster <zhangminke@51yund.com>
2016-10-31 21:09:47 +0800

    save

---
+++ ios_sdk/work_space/hardware_sdk/Products/hardware_sdk.framework/Headers/hardware_sdk.h
@@ -0,0 +1,19 @@
+//
+//  hardware_sdk.h
+//  hardware_sdk
+//
+//  Created by 张旻可 on 2016/10/31.
+//  Copyright © 2016年 yuedong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for hardware_sdk.
+FOUNDATION_EXPORT double hardware_sdkVersionNumber;
+
+//! Project version string for hardware_sdk.
+FOUNDATION_EXPORT const unsigned char hardware_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <hardware_sdk/PublicHeader.h>
+
+
---
+++ ios_sdk/work_space/hardware_sdk/third_part/yd_fmdb/YDFMDB.h
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double YDFMDBVersionNumber;
+FOUNDATION_EXPORT const unsigned char YDFMDBVersionString[];
+
+#import "YDFMDatabase.h"
+#import "YDFMResultSet.h"
+#import "YDFMDatabaseAdditions.h"
+#import "YDFMDatabaseQueue.h"
+#import "YDFMDatabasePool.h"
